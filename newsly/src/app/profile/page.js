7e33912fddad3648496585516805ceeafe8ae6d3
karthickprabakaran <karthickprabakaran06@gmail.com>
2025-12-03 09:01:40 +0530

    Fixed the import path in the page.js

--- newsly/src/app/profile/page.js
+++ newsly/src/app/profile/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { supabase } from "../../../util/supabase"; // client anon key
+import { supabase } from "../../../../util/supabase"; // client anon key
 
 const PREDEFINED_CATEGORIES = [
   "sports",
